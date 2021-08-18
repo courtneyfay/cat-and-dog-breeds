14ac797e7bd2c7b42bf727ea27c62e58d9e9eda6
Courtney Fay <courtneyfay@users.noreply.github.com>
2021-08-18 08:54:38 -0600

    Adding styled-components library + basic header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Header from './components/Header';
 import PetType from './components/PetType';
 import BreedSearch from './components/BreedSearch';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  background: #064574;
+  width: 100vw;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <>
+    <Background>
       <Header />
       <PetType />
       <BreedSearch />
-    </>
+    </Background>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+import Logo from '../Logo.svg';
+
+const Wrapper = styled.header`
+    background-color: white;
+    padding: 27px 0px;
+    display: flex;
+    justify-content: center;
+`;
+
 const Header = () => {
-    return <span>Header here</span>
+    return (
+        <Wrapper>
+            <img src={Logo} alt="logo" />
+        </Wrapper>
+    )
 }
 
 export default Header;
